241390d6d111118a958a21a0fa54f53cc9617a59
Danny Oh <dannyoh0316@gmail.com>
2021-01-01 17:43:06 -0600

    course list

--- App.tsx
+++ App.tsx
@@ -1,15 +1,78 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const schedule = {
-  title: 'CS Courses for 2018 - 2019'
+  "title": "CS Courses for 2018-2019",
+  "courses": [
+    {
+      "id": "F101",
+      "title": "Computer Science: Concepts, Philosophy, and Connections",
+      "meets": "MWF 11:00-11:50"
+    },
+    {
+      "id": "F110",
+      "title": "Intro Programming for non-majors",
+      "meets": "MWF 10:00-10:50"
+    },
+    {
+      "id": "F111",
+      "title": "Fundamentals of Computer Programming I",
+      "meets": "MWF 13:00-13:50"
+    },
+    {
+      "id": "F211",
+      "title": "Fundamentals of Computer Programming II",
+      "meets": "TuTh 12:30-13:50"
+    }
+  ]
 };
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.bannerStyle}>{schedule.title}</Text>
+const Banner: React.FC<{ title: string }> = ({ title }) => (
+  <Text style={styles.banner}>{ title }</Text>
+);
+
+interface CourseProps {
+  course: {
+    id: string;
+    title: string;
+    meets: string;
+  };
+};
+
+const getCourseNumber = course => (
+  course.id.slice(1)
+);
+
+const Course: React.FC<CourseProps> = ({ course }) => (
+  <TouchableOpacity style={styles.courseButton}>
+    <Text style={styles.courseText}>
+      {`CS ${getCourseNumber(course)}\n${course.meets}`}
+    </Text>
+  </TouchableOpacity>
+);
+
+interface CourseListProps {
+  courses: {
+    id: string;
+    title: string;
+    meets: string;
+  }[]
+};
+
+const CourseList: React.FC<CourseListProps> = ({ courses }) => (
+  <ScrollView>
+    <View style={styles.courseList}>
+      {courses.map(course => <Course key={course.id} course={course} />)}
     </View>
+  </ScrollView>
+);
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Banner title={schedule.title} />
+      <CourseList courses={schedule.courses} />
+    </SafeAreaView>
   );
 };
 
@@ -18,10 +81,34 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 20,
   },
-  bannerStyle: {
+  banner: {
     color: '#888',
     fontSize: 32,
+  },
+  courseList: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  courseButton: {
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    height: 60,
+    padding: 10,
+    minWidth: 90,
+    maxWidth: 90,
+    backgroundColor: '#66b0ff',
+  },
+  courseText:{
+    color: '#fff',
+    fontSize: 12,
+    textAlign: 'center',
   },
 });
 
