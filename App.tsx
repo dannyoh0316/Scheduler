5c5687ed58483ed30e5cc69736d2680f93198063
Danny Oh <dannyoh0316@gmail.com>
2021-01-01 16:15:58 -0600

    initial app

--- App.tsx
+++ App.tsx
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textStyle}>Welcome to my app!</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00f',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textStyle: {
+    color: '#fff',
+    fontSize: 32,
   },
 });
